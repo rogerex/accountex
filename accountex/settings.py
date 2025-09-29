89e7192f5ffd98d076f7f190d9b41e6be593b3c1
Roger Limachi <rogerex.bo@gmail.com>
2025-09-29 10:59:26 -0400

    feat: css for account type report

--- accountex/settings.py
+++ accountex/settings.py
@@ -139,7 +139,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    'financial'
+    'financial',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
