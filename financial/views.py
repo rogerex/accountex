46008460f6d4d8d609e9d1d95c2285c058ea1032
Roger Limachi <rogerex.bo@gmail.com>
2025-10-03 08:04:59 -0400

    chore: little changes

--- financial/views.py
+++ financial/views.py
@@ -189,7 +189,14 @@ ORDER BY b.account_name
       if len(viewCurrencyTagGroup.tagGroups) > 0 and viewCurrencyTagGroup.total != 0:
           viewCurrencyTagGroups.append(viewCurrencyTagGroup)
 
-  context = { 'rows': patrimonyRows, 'datetime': datetime.today(), 'deletedRows': deletedRows, 'ignoredRows': ignoredRows, 'currencyTagGroups': currencyTagGroups, 'viewCurrencyTagGroups': viewCurrencyTagGroups }
+  context = {
+      'rows': patrimonyRows,
+      'datetime': datetime.today(),
+      'deletedRows': deletedRows,
+      'ignoredRows': ignoredRows,
+      'currencyTagGroups': currencyTagGroups,
+      'viewCurrencyTagGroups': viewCurrencyTagGroups,
+  }
 
   # pdb.set_trace()
   return render(request, 'reports/patrimony.html', context)
