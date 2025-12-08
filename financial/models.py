b2df6a08f4e870479499989dbeaa5b6359778d2f
Roger Limachi <rogerex.bo@gmail.com>
2025-12-08 08:18:53 -0400

    feat: move json model to another file

--- financial/models.py
+++ financial/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.utils.html import escape, format_html
 import datetime
 from django import forms
+from financial.seat_initial_default import INITIAL_SEAT_DETAIL
 
 # Create your models here.
 STATUS = (
@@ -269,61 +270,6 @@ class SeatDetail(models.Model):
         db_table = 'seat_detail'
     def __unicode__(self): 
         return ' '.join([self.seat.code, str(self.mount), '>>>Debit:', self.debitAccount.name, 'Credit:', self.creditAccount.name])
-
-INITIAL_SEAT_DETAIL = [
-    ### Fondo de emergencia (Intereses ganados)
-    {'debitAccount': 205, 'creditAccount': 148, 'description': '% Aave Coinbase Wallet (Delete)', 'mount': 4, },
-    {'debitAccount': 155, 'creditAccount': 148, 'description': '% Staking Coinbase Exchange (Delete)', 'mount': 4, },
-    {'debitAccount': 146, 'creditAccount': 148, 'description': '% Staking Kraken Exchange (Delete)', 'mount': 4, },
-
-    ### Fondo de ahorros para alguna de mis objetos de consumo (Intereses ganados)
-    {'debitAccount': 145, 'creditAccount': 148, 'description': '% Defi Blend Meru Wallet (Delete)', 'mount': 4, },
-
-    ### Fondo de IT (Intereses ganados), yo creo en un futuro me voy a comprar una casa
-    #{'debitAccount': 157, 'creditAccount': 148, 'description': '% Aave Coinbase Wallet (Delete)', 'mount': 4, },
-    #{'debitAccount': 154, 'creditAccount': 148, 'description': '% Defi Nebeus Wallet (Delete)', 'mount': 4, },
-    #{'debitAccount': 153, 'creditAccount': 148, 'description': '% Staking Nexo Exchange (Delete)', 'mount': 0, },
-
-    ### Bancos (Intereses ganados)
-    {'debitAccount': 206, 'creditAccount': 26, 'description': '% BB (Delete)', 'mount': 1, },
-    {'debitAccount': 124, 'creditAccount': 26, 'description': '% BE (Delete)', 'mount': 1, },
-    {'debitAccount': 156, 'creditAccount': 26, 'description': '% BF (Delete)', 'mount': 1, },
-    {'debitAccount': 158, 'creditAccount': 26, 'description': '% BG (Delete)', 'mount': 1, },
-    {'debitAccount': 170, 'creditAccount': 26, 'description': '% BG+ (Delete)', 'mount': 1, },
-    {'debitAccount': 65, 'creditAccount': 26, 'description': '% BMSC (Delete)', 'mount': 1, },
-    {'debitAccount': 43, 'creditAccount': 26, 'description': '% BNB (Delete)', 'mount': 0, },
-    {'debitAccount': 143, 'creditAccount': 26, 'description': '% BS (Delete)', 'mount': 1, },
-    {'debitAccount': 57, 'creditAccount': 26, 'description': '% BU (Delete)', 'mount': 1, },
-    {'debitAccount': 115, 'creditAccount': 26, 'description': '% BCP (Delete)', 'mount': 1, },
-
-    ### Fondo de Ahorro Mensual (Intereses ganados)
-    {'debitAccount': 160, 'creditAccount': 148, 'description': '% Staking Binance Exchange (Delete)', 'mount': 4, },
-
-    ### Retiro de dinero para gastos de mes
-    {'debitAccount': 167, 'creditAccount': 161, 'description': 'Retiro de p2p para presupuesto de mes (Delete)', 'mount': 1100, },
-    {'debitAccount': 158, 'creditAccount': 18, 'description': 'Esos USDT de arriba (Delete)', 'mount': 16000, },
-
-    ### Distribucion de presupuesto de mes
-    {'debitAccount': 143, 'creditAccount': 158, 'description': 'Presupuesto Mensual (Delete)', 'mount': 15000, }, #BS
-
-    # Distribucion en bancos
-    {'debitAccount': 124, 'creditAccount': 143, 'description': 'Gastos - Streaming (Netflix) (Delete)', 'mount': 55, },      #BE
-    {'debitAccount': 124, 'creditAccount': 143, 'description': 'Gastos - Supervivencia (Delete)', 'mount': 2000, },          #BE
-    {'debitAccount': 156, 'creditAccount': 143, 'description': 'Gastos - Aporte Gustos (Delete)', 'mount': 2200, },          #BF
-    {'debitAccount': 170, 'creditAccount': 143, 'description': 'Gastos - Expensas (Delete)', 'mount': 1000, },               #BG+
-    {'debitAccount': 65, 'creditAccount': 143, 'description': 'Gastos - Aporte Pareja (Delete)', 'mount': 1000, },           #BMSC
-    {'debitAccount': 43, 'creditAccount': 143, 'description': 'Gastos - Aporte Padres (Delete)', 'mount': 2000, },           #BNB
-    {'debitAccount': 57, 'creditAccount': 143, 'description': 'Gastos - Yo (Delete)', 'mount': 1500, },                      #BU
-    {'debitAccount': 115, 'creditAccount': 143, 'description': 'Gastos - Aporte Comidas Familia (Delete)', 'mount': 1000, }, #BCP
-
-    # Distribucion externa, mi hermana
-    {'debitAccount': 196, 'creditAccount': 143, 'description': 'Gastos - Aporte hermana menor (Delete)', 'mount': 600, }, #BNB de mi hermana
-
-    # Distribucion en billeteras
-    {'debitAccount': 200, 'creditAccount': 143, 'description': 'Gastos - Terapia (Delete)', 'mount': 400, },         #Billetera Altoke
-    {'debitAccount': 119, 'creditAccount': 143, 'description': 'Gastos - Servicios fijos (Delete)', 'mount': 500, }, #Billetera Yape
-    {'debitAccount': 208, 'creditAccount': 143, 'description': 'Gastos - Mascotas (Delete)', 'mount': 400, },        #Billetera Yolo
-]
 
 queryParamForDefault = 'initial'
 
---
+++ financial/seat_initial_default.py
@@ -0,0 +1,55 @@
+INITIAL_SEAT_DETAIL = [
+    ### Fondo de emergencia (Intereses ganados)
+    {'debitAccount': 205, 'creditAccount': 148, 'description': '% Aave Coinbase Wallet (Delete)', 'mount': 4, },
+    {'debitAccount': 155, 'creditAccount': 148, 'description': '% Staking Coinbase Exchange (Delete)', 'mount': 4, },
+    {'debitAccount': 146, 'creditAccount': 148, 'description': '% Staking Kraken Exchange (Delete)', 'mount': 4, },
+
+    ### Fondo de ahorros para alguna de mis objetos de consumo (Intereses ganados)
+    {'debitAccount': 168, 'creditAccount': 148, 'description': '% Defi Blend Airtm Wallet (Delete)', 'mount': 4, },
+
+    ### Fondo de IT (Intereses ganados), yo creo en un futuro me voy a comprar una casa
+    #{'debitAccount': 157, 'creditAccount': 148, 'description': '% Aave Coinbase Wallet (Delete)', 'mount': 4, },
+    #{'debitAccount': 154, 'creditAccount': 148, 'description': '% Defi Nebeus Wallet (Delete)', 'mount': 4, },
+    #{'debitAccount': 153, 'creditAccount': 148, 'description': '% Staking Nexo Exchange (Delete)', 'mount': 0, },
+
+    ### Bancos (Intereses ganados)
+    #{'debitAccount': 206, 'creditAccount': 26, 'description': '% BB (Delete)', 'mount': 1, }, # Deshabilitado hasta que pueda mover dinero de mi madre de aqui
+    {'debitAccount': 124, 'creditAccount': 26, 'description': '% BE (Delete)', 'mount': 1, },
+    {'debitAccount': 156, 'creditAccount': 26, 'description': '% BFI (Delete)', 'mount': 1, },
+    {'debitAccount': 219, 'creditAccount': 26, 'description': '% BFO (Delete)', 'mount': 1, },
+    {'debitAccount': 158, 'creditAccount': 26, 'description': '% BG (Delete)', 'mount': 1, },
+    {'debitAccount': 170, 'creditAccount': 26, 'description': '% BG+ (Delete)', 'mount': 1, },
+    #{'debitAccount': 65, 'creditAccount': 26, 'description': '% BMSC (Delete)', 'mount': 1, }, # Deshabilitado hasta que gaste todo del anticretico
+    {'debitAccount': 43, 'creditAccount': 26, 'description': '% BNB (Delete)', 'mount': 0, },
+    {'debitAccount': 143, 'creditAccount': 26, 'description': '% BSO (Delete)', 'mount': 1, },
+    {'debitAccount': 57, 'creditAccount': 26, 'description': '% BU (Delete)', 'mount': 1, },
+    {'debitAccount': 115, 'creditAccount': 26, 'description': '% BCP (Delete)', 'mount': 1, },
+
+    ### Fondo de Ahorro Mensual (Intereses ganados)
+    {'debitAccount': 160, 'creditAccount': 148, 'description': '% Staking Binance Exchange (Delete)', 'mount': 4, },
+
+    ### Retiro de dinero para gastos de mes
+    {'debitAccount': 167, 'creditAccount': 161, 'description': 'Retiro de p2p para presupuesto de mes (Delete)', 'mount': 1100, },
+    {'debitAccount': 158, 'creditAccount': 18, 'description': 'Esos USDT de arriba (Delete)', 'mount': 16000, },
+
+    ### Distribucion de presupuesto de mes
+    {'debitAccount': 143, 'creditAccount': 158, 'description': 'Presupuesto Mensual (Delete)', 'mount': 15000, }, #BSO
+
+    # Distribucion en bancos
+    {'debitAccount': 124, 'creditAccount': 143, 'description': 'Gastos - Streaming (Netflix) (Delete)', 'mount': 55, },      #BE
+    {'debitAccount': 124, 'creditAccount': 143, 'description': 'Gastos - Supervivencia (Delete)', 'mount': 2000, },          #BE
+    {'debitAccount': 156, 'creditAccount': 143, 'description': 'Gastos - Aporte Gustos (Delete)', 'mount': 2200, },          #BFI
+    {'debitAccount': 170, 'creditAccount': 143, 'description': 'Gastos - Expensas (Delete)', 'mount': 1000, },               #BG+
+    {'debitAccount': 65, 'creditAccount': 143, 'description': 'Gastos - Aporte Pareja (Delete)', 'mount': 1000, },           #BMSC
+    {'debitAccount': 43, 'creditAccount': 143, 'description': 'Gastos - Aporte Padres (Delete)', 'mount': 2000, },           #BNB
+    {'debitAccount': 57, 'creditAccount': 143, 'description': 'Gastos - Yo (Delete)', 'mount': 1500, },                      #BU
+    {'debitAccount': 115, 'creditAccount': 143, 'description': 'Gastos - Aporte Comidas Familia (Delete)', 'mount': 1000, }, #BCP
+
+    # Distribucion externa, mi hermana
+    {'debitAccount': 196, 'creditAccount': 143, 'description': 'Gastos - Aporte hermana menor (Delete)', 'mount': 500, }, #BNB de mi hermana
+
+    # Distribucion en billeteras
+    {'debitAccount': 200, 'creditAccount': 143, 'description': 'Gastos - Terapia (Delete)', 'mount': 400, },         #Billetera Altoke
+    {'debitAccount': 119, 'creditAccount': 143, 'description': 'Gastos - Servicios fijos (Delete)', 'mount': 500, }, #Billetera Yape
+    {'debitAccount': 208, 'creditAccount': 143, 'description': 'Gastos - Mascotas (Delete)', 'mount': 400, },        #Billetera Yolo
+]
