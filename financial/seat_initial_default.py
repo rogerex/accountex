83922cf693a71c0eaa1274ab2e9601cbac2d8a39
Roger Limachi <rogerex.bo@gmail.com>
2026-08-17 11:17:31 -0400

    chore: update rows of seat

--- financial/seat_initial_default.py
+++ financial/seat_initial_default.py
@@ -13,44 +13,52 @@ INITIAL_SEAT_DETAIL = [
     #{'debitAccount': 153, 'creditAccount': 148, 'description': '% Staking Nexo Exchange (Delete)', 'mount': 0, },
 
     ### Bancos (Intereses ganados)
-    #{'debitAccount': 206, 'creditAccount': 26, 'description': '% BB (Delete)', 'mount': 1, }, # Deshabilitado hasta que pueda mover dinero de mi madre de aqui
+    {'debitAccount': 206, 'creditAccount': 26, 'description': '% BB (Delete)', 'mount': 1, },
+    {'debitAccount': 246, 'creditAccount': 26, 'description': '% BEC (Delete)', 'mount': 1, },
     {'debitAccount': 124, 'creditAccount': 26, 'description': '% BE (Delete)', 'mount': 1, },
     {'debitAccount': 156, 'creditAccount': 26, 'description': '% BFI (Delete)', 'mount': 1, },
     {'debitAccount': 219, 'creditAccount': 26, 'description': '% BFO (Delete)', 'mount': 1, },
     {'debitAccount': 158, 'creditAccount': 26, 'description': '% BG (Delete)', 'mount': 1, },
     {'debitAccount': 170, 'creditAccount': 26, 'description': '% BG+ (Delete)', 'mount': 1, },
-    #{'debitAccount': 65, 'creditAccount': 26, 'description': '% BMSC (Delete)', 'mount': 1, }, # Deshabilitado hasta que gaste todo del anticretico
-    {'debitAccount': 43, 'creditAccount': 26, 'description': '% BNB (Delete)', 'mount': 0, },
+    {'debitAccount': 65, 'creditAccount': 26, 'description': '% BMSC (Delete)', 'mount': 1, },
+    {'debitAccount': 43, 'creditAccount': 26, 'description': '% BNB (Delete)', 'mount': 1, },
     {'debitAccount': 143, 'creditAccount': 26, 'description': '% BSO (Delete)', 'mount': 1, },
     {'debitAccount': 57, 'creditAccount': 26, 'description': '% BU (Delete)', 'mount': 1, },
     {'debitAccount': 115, 'creditAccount': 26, 'description': '% BCP (Delete)', 'mount': 1, },
+
+    ### Billeteras (Intereses ganados)
+    {'debitAccount': 200, 'creditAccount': 26, 'description': '% AlToke (Delete)', 'mount': 1, },
 
     ### Fondo de Ahorro Mensual (Intereses ganados)
     #{'debitAccount': 160, 'creditAccount': 148, 'description': '$ % Staking Binance Exchange (Delete)', 'mount': 4, },  # Deshabilitado hasta que gaste todo del anticretico
 
     ### Retiro de dinero para gastos de mes
-    #{'debitAccount': 167, 'creditAccount': 161, 'description': 'Retiro de p2p para presupuesto de mes (Delete)', 'mount': 1300, },  # Deshabilitado hasta que gaste todo del anticretico
-    {'debitAccount': 158, 'creditAccount': 18, 'description': 'Ingresos: Esos USDT de arriba (Delete)', 'mount': 13000, },
+    {'debitAccount': 167, 'creditAccount': 161, 'description': 'Retiro de p2p para presupuesto de mes (Delete)', 'mount': 990, },  # Deshabilitado hasta que gaste todo del anticretico
+    {'debitAccount': 158, 'creditAccount': 18, 'description': 'Ingresos: Esos USDT de arriba (Delete)', 'mount': 16000, },
+    {'debitAccount': 158, 'creditAccount': 18, 'description': 'Ingresos: Esos USDT de arriba (Delete)', 'mount': 16000, },
 
     ### Distribucion de presupuesto de mes
-    {'debitAccount': 143, 'creditAccount': 158, 'description': 'Presupuesto Mensual (Delete)', 'mount': 12000, }, #BSO
+    {'debitAccount': 143, 'creditAccount': 158, 'description': 'Presupuesto Mensual (Delete)', 'mount': 15000, }, #BSO
 
     # Distribucion en bancos
-    {'debitAccount': 124, 'creditAccount': 143, 'description': 'Gastos - Streaming (Netflix) (Delete)', 'mount': 55, },       #BE
-    {'debitAccount': 124, 'creditAccount': 143, 'description': 'Gastos - Supervivencia (Delete)', 'mount': 2000, },           #BE
-    {'debitAccount': 156, 'creditAccount': 143, 'description': 'Gastos - Aporte Gustos (Delete)', 'mount': 2200, },           #BFI
-    {'debitAccount': 170, 'creditAccount': 143, 'description': 'Gastos - Expensas (Delete)', 'mount': 500, },                 #BG+
-    {'debitAccount': 65, 'creditAccount': 143, 'description': 'Gastos - Aporte Pareja (Delete)', 'mount': 1000, },            #BMSC
-    {'debitAccount': 43, 'creditAccount': 143, 'description': 'Gastos - Aporte Padres (Delete)', 'mount': 2000, },            #BNB
+    {'debitAccount': 206, 'creditAccount': 143, 'description': 'Liquidez (Delete)', 'mount': 200, },                          #BB
+    {'debitAccount': 124, 'creditAccount': 143, 'description': 'Gastos - Streaming (Netflix) (Delete)', 'mount': 80, },       #BE
+    {'debitAccount': 124, 'creditAccount': 143, 'description': 'Gastos - Supervivencia (Delete)', 'mount': 2300, },           #BE
+    {'debitAccount': 156, 'creditAccount': 143, 'description': 'Gastos - Aporte Gustos (Delete)', 'mount': 3200, },           #BFI
+    {'debitAccount': 170, 'creditAccount': 143, 'description': 'Gastos - Expensas (Delete)', 'mount': 510, },                 #BG+
+    {'debitAccount': 65, 'creditAccount': 143, 'description': 'Gastos - Aporte Citas (Delete)', 'mount': 1500, },             #BMSC
+    {'debitAccount': 43, 'creditAccount': 143, 'description': 'Gastos - Aporte Padres (Delete)', 'mount': 3000, },            #BNB
     {'debitAccount': 57, 'creditAccount': 143, 'description': 'Gastos - Yo (Delete)', 'mount': 1500, },                       #BU
     {'debitAccount': 115, 'creditAccount': 143, 'description': 'Gastos - Aporte Comidas Familia (Delete)', 'mount': 1000, },  #BCP
 
-    # Distribucion externa, mi hermana
+    # Distribucion externas
     {'debitAccount': 196, 'creditAccount': 143, 'description': 'Gastos - Aporte hermana menor (Delete)', 'mount': 500, }, #BNB de mi hermana
+    {'debitAccount': 73, 'creditAccount': 143, 'description': 'Gastos - Aporte Paola (Delete)', 'mount': 500, },          #BG de paola
 
     # Distribucion en billeteras
-    {'debitAccount': 200, 'creditAccount': 143, 'description': 'Gastos - Salud (Delete)', 'mount': 200, },           #Billetera Altoke
+    {'debitAccount': 200, 'creditAccount': 143, 'description': 'Gastos - Salud (Delete)', 'mount': 680, },           #Billetera Altoke
     {'debitAccount': 119, 'creditAccount': 143, 'description': 'Gastos - Servicios fijos (Delete)', 'mount': 300, }, #Billetera Yape
+    {'debitAccount': 241, 'creditAccount': 143, 'description': 'Gastos - Paola (Delete)', 'mount': 1000, },          #Billetera Yasta
     {'debitAccount': 208, 'creditAccount': 143, 'description': 'Gastos - Mascotas (Delete)', 'mount': 400, },        #Billetera Yolo
 ]
 
